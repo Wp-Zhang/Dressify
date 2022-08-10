32c3a57043eb91cd39e2b992193d246991a4d836
wpzhang <wp.zhang@hotmail.com>
2022-08-10 17:12:28 -0400

    :lipstick: add checkout success text

--- frontend/src/components/CartPage/CheckoutSuccessPage.js
+++ frontend/src/components/CartPage/CheckoutSuccessPage.js
@@ -1,4 +1,4 @@
-import { Button, Modal, Image } from '@nextui-org/react';
+import { Button, Modal, Image, Text, Spacer, Container } from '@nextui-org/react';
 import { useNavigate } from 'react-router-dom';
 import confetti from 'canvas-confetti';
 
@@ -24,15 +24,27 @@ function CheckoutPopover({ visible, setVisible }) {
                 }
             }
         >
-            <Modal.Header justify='center'>
+            <Modal.Header justify='center' style={{ paddingBottom: 0 }}>
                 <Image src="./logo.png" alt="products logo" width={90} />
             </Modal.Header>
             <Modal.Body css={{ textAlign: "center" }}>
-                <Button onClick={() => {
-                    navigate("/order")
-                }}>
-                    Jump to "My Order" page
-                </Button>
+                <Text h4 style={{ fontFamily: "Montserrat-SemiBold" }}>
+                    Thank you for your order!
+                </Text>
+                <Spacer y={0.1} />
+                <Container display='flex' justify='center'>
+                    <Button
+                        auto
+                        flat
+                        color="warning"
+                        onClick={() => {
+                            navigate("/order")
+                        }}
+                    >
+                        Jump to "My Order"
+                    </Button>
+                </Container>
+                <Spacer y={0.1} />
             </Modal.Body>
         </Modal>
     );
